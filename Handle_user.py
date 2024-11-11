43a6c662a09876b5ad4bf4fc6cbb4bab8135f8b1
ChetanChoudhary007 <cc393653@gmail.com>
2024-11-11 10:10:31 +0530

    add: requirements.txt file

--- Handle_user.py
+++ Handle_user.py
@@ -4,7 +4,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from dotenv import load_dotenv
-import os
 
 # load all environment variables
 load_dotenv()
